0e5003672eeaa51dbb51bc4711f0b97bc01a925b
RaphaelNoriega <jraphaelnm@gmail.com>
2023-12-07 09:29:30 -0700

    cambios en el juego del gato

--- src/App.java
+++ src/App.java
@@ -1,46 +1,94 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import java.awt.*;
+import javax.swing.ImageIcon;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-//import java.awt.Color;
 public class App extends JFrame implements Runnable{
 
     int width = 800;
     int height = 800;
     int tcelda = 150;
     int desplazamiento = 170;
-
+    Control control;
+    
+    Image cross=this.cargarImagen("cross.png");
+    Image circle=this.cargarImagen("circle.png");
 
     BufferedImage bi = new BufferedImage(this.width,this.height,BufferedImage.TYPE_4BYTE_ABGR);
     Graphics2D g2d = (Graphics2D) bi.getGraphics();
+    ArrayList<myRectangle> zonapulsable = new ArrayList<myRectangle>();
    
+    public Image cargarImagen(String nombre){
+        Image image=new ImageIcon(this.getClass().getResource(nombre)).getImage();
+        return image;
+    }
+
+    public void crearZonaPulsable(Graphics g){
+        for (int i = 0; i < control.tablero[0].length; i++) {
+            for (int j = 0; j < control.tablero.length; j++) {
+              Rectangle2D rect = new Rectangle(desplazamiento+(i*tcelda),desplazamiento+(j*tcelda),tcelda,tcelda);
+              myRectangle mrect = new myRectangle(rect,i,j);
+              this.zonapulsable.add(mrect);
+            }
+         }
+    }
+
+    public void pintarCasillas(Graphics g){
+        Graphics2D g2d2 = (Graphics2D) g;
+        for(myRectangle mrect: control.getLcasillas()){
+            g2d2.setColor(Color.WHITE);
+            g2d2.draw(mrect.getRect());
+        }
+    }
+
+    public void píntarFondo(Graphics g){
+        for (int i = 0; i < 100; i++) {
+            Color c = new Color(34+i, 53, 177  );
+            g.setColor(c);
+            g.fillRect(0, i+8, this.width, this.height);
+        }
+    }
+
+    public void pintarSimbolo(Graphics g){
+        for (int i = 0; i < control.tablero[0].length; i++) {
+            for (int j = 0; j < control.tablero.length; j++) {
+              if (control.tablero[i][j] == 0) {
+                g.drawImage(circle, desplazamiento+(i*tcelda),desplazamiento+(j*tcelda),tcelda,tcelda,this);
+              }
+                if (control.tablero[i][j] == 1) {
+                    g.drawImage(cross, desplazamiento+(i*tcelda),desplazamiento+(j*tcelda),tcelda,tcelda,this);
+              }
+            }
+         }
+    }
+
+    @Override
+    public void paint(Graphics g){
+        this.píntarFondo(g2d);
+        this.pintarCasillas(g2d);
+        this.pintarSimbolo(g2d);
+        g.drawImage(bi,10,10,this.width,this.height,this);
+
+    }
+    
     //constructor
     public App(){
         this.setSize(this.height,this.width);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("EL JUEGO DEL GATO");
+        control = new Control();
+        this.crearZonaPulsable(g2d);
+        control.setLcasillas(zonapulsable);
+        this.addMouseListener(control);
+        Thread hilo = new Thread(this);
+        hilo.start();
     }
 
-    public void píntarFondo(Graphics g){
-        for (int i = 0; i < 100; i++) {
-            Color c = new Color(26+i, 188, 156);
-            g.setColor(c);
-            g.fillRect(0, i+8, this.width, this.height);
-        }
-    }
-
-    public void pintarSimbolo(){
-
-    }
-
-    public void paint(Graphics g){
-        this.píntarFondo(g2d);
-        g.drawImage(bi,10,10,this.width,this.height,this);
-
-    }
-    
     public static void main(String[] args) throws Exception {
         System.out.println("Iniciando juego");
         App juego = new App();
--- src/Control.java
+++ src/Control.java
@@ -1,20 +1,56 @@
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.Point;
 import java.util.ArrayList;
+import java.awt.geom.Point2D;
 
 public class Control implements MouseListener{
 
    final int size = 3;
    int[][] tablero = new int[size][size];
+   int boton;
    ArrayList<myRectangle> zonapulsable = new ArrayList<myRectangle>();
 
    //constructor
    public Control(){
-
+      this.crearTablero();
    }
 
+   public void crearTablero(){
+      for (int i = 0; i < tablero[0].length; i++) {
+         for (int j = 0; j < tablero.length; j++) {
+            tablero[i][j] = -1;
+         }
+      }
+   }
    
+   public void getCasilla(Point punto){
+      Point2D p2d = punto;
+      for(myRectangle mrect:this.getLcasillas()){
+         if(mrect.getRect().contains(p2d)){
+            /* System.out.println("tablero["+mrect.getPosx()+ "]["+mrect.getPosy()+"]"); */
+            System.out.println("tablero["+mrect.getPosy()+ "]["+mrect.getPosx()+"]");
+            if (boton == 1) {
+            System.out.println("BOTON IZQUIERDO");
+               tablero[mrect.getPosx()][mrect.getPosy()]=0;
+            }
+            if (boton == 3) {
+               System.out.println("BOTON DERECHO");
+                tablero[mrect.getPosx()][mrect.getPosy()]=1;
+            }
+         }
+      }
+   }
+   
+   public ArrayList<myRectangle> getLcasillas() {
+      return zonapulsable;
+   }
+
+   public void setLcasillas(ArrayList<myRectangle> zonapulsable) {
+      this.zonapulsable = zonapulsable;
+   }
+
    public int[][] getTablero() {
       return tablero;
    }
@@ -25,7 +61,7 @@ public class Control implements MouseListener{
    }
 
 
-
+   //metodos abstractos
    @Override
    public void mouseClicked(MouseEvent e) {
      
@@ -33,7 +69,10 @@ public class Control implements MouseListener{
 
    @Override
    public void mousePressed(MouseEvent e) {
-      
+      Point punto = e.getPoint();
+      boton = e.getButton();
+      System.out.println("estas presionando este boton "+boton);
+      this.getCasilla(punto);
    }
 
    @Override
